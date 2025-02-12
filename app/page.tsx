2b5f79aa7e61bb84d51ab1548ffdca5522a807b1
Tamás Szita <szita999@gmail.com>
2025-02-13 00:12:56 +0100

    initial commit, bal kártya kész van

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,91 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { IoLocation, IoMail } from "react-icons/io5";
+import { TbWorldWww } from "react-icons/tb";
+import portrait from "@/public/me.jpeg";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-complex-radial min-h-screen flex flex-row items-center p-8 justify-center gap-24">
+      <div className="bg-[#F5F5F5] rounded-2xl w-[400px]  p-10 flex flex-col items-center ">
+        <div className="relative -mt-[7.5rem] ">
+          <Image
+            src={portrait}
+            width={160}
+            height={160}
+            alt="selfportrait"
+            className="rounded-2xl"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <h1 className="text-[#3D3E41] font-extrabold text-2xl pt-4">
+          Szita Tamás
+        </h1>
+        <p className="text-[#A1A1A1] pt-1">Webfejlesztő</p>
+
+        <div className="flex flex-row gap-2 pb-10 pt-3">
+          <Link href="https://github.com/Medivessing" passHref legacyBehavior>
+            <a target="_blank" className="bg-[#EAEAEA]  p-1 rounded hover:bg-[#B5C18E] ">
+              <FaGithub size={23} color="#3D3E41" />
+            </a>
+          </Link>
+
+          <Link href="https://tamasszita.com" passHref legacyBehavior>
+            <a target="_blank" className="bg-[#EAEAEA]  p-1 rounded hover:bg-[#B5C18E]">
+              <TbWorldWww size={23} color="#3D3E41" />
+            </a>
+          </Link>
+
+          <Link
+            href="https://www.linkedin.com/in/tamasszita/"
+            passHref
+            legacyBehavior
+          >
+            <a target="_blank" className="bg-[#EAEAEA]  p-1 rounded hover:bg-[#B5C18E]">
+              <FaLinkedin size={23} color="#3D3E41" />
+            </a>
+          </Link>
+        </div>
+
+        <div className=" bg-[#EAEAEA] rounded-2xl flex flex-col w-[300px] gap-5 p-4 justify-between ">
+          <div className="flex flex-row gap-4 items-center">
+            <div className="bg-[#f5f5f5] shadow-md p-1 rounded">
+              <IoMail size={28} color="#3D3E41" />
+            </div>
+            <div className="flex flex-col">
+              <p className="text-xs  text-[#3D3E41]">Email</p>
+              <p className=" text-[#3D3E41]">szita999@gmail.com</p>
+            </div>
+          </div>
+
+          <hr className="h-[2px] bg-[#A1A1A1]"/>
+
+          <div className="flex flex-row gap-4 items-center">
+            <div className="bg-[#f5f5f5] shadow-md p-1 rounded">
+              <IoLocation size={28} color="#3D3E41" />
+            </div>
+            <div className="flex flex-col">
+              <p className="text-xs text-[#3D3E41]">Location</p>
+              <p className=" text-[#3D3E41]">Remote / Balatonfüred</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="pt-10">
+          <button className="bg-gradient-to-r from-[#6C7D47] to-[#A3B678] py-1 px-6 rounded-2xl text-lg text-[#f5f5f5] hover:text-[#3D3E41]">
+            Önéletrajz letöltése
+          </button>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <div className="bg-[#F5F5F5] rounded-2xl w-[420px] h-[120px] ml-auto p-6">
+          navbar
+        </div>
+        <div className="bg-[#F5F5F5] rounded-2xl w-[700px] h-[600px] p-6">
+          text
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,57 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "complex-radial": `radial-gradient(circle at 29% 55%, hsla(329,0%,99%,0.05) 0%, hsla(329,0%,99%,0.05) 4%,transparent 4%, transparent 44%,transparent 44%, transparent 100%),
+          radial-gradient(circle at 85% 89%, hsla(329,0%,99%,0.05) 0%, hsla(329,0%,99%,0.05) 51%,transparent 51%, transparent 52%,transparent 52%, transparent 100%),
+          radial-gradient(circle at 6% 90%, hsla(329,0%,99%,0.05) 0%, hsla(329,0%,99%,0.05) 53%,transparent 53%, transparent 64%,transparent 64%, transparent 100%),
+          radial-gradient(circle at 35% 75%, hsla(329,0%,99%,0.05) 0%, hsla(329,0%,99%,0.05) 6%,transparent 6%, transparent 98%,transparent 98%, transparent 100%),
+          radial-gradient(circle at 56% 75%, hsla(329,0%,99%,0.05) 0%, hsla(329,0%,99%,0.05) 16%,transparent 16%, transparent 23%,transparent 23%, transparent 100%),
+          radial-gradient(circle at 42% 0%, hsla(329,0%,99%,0.05) 0%, hsla(329,0%,99%,0.05) 3%,transparent 3%, transparent 26%,transparent 26%, transparent 100%),
+          radial-gradient(circle at 29% 28%, hsla(329,0%,99%,0.05) 0%, hsla(329,0%,99%,0.05) 51%,transparent 51%, transparent 75%,transparent 75%, transparent 100%),
+          radial-gradient(circle at 77% 21%, hsla(329,0%,99%,0.05) 0%, hsla(329,0%,99%,0.05) 35%,transparent 35%, transparent 55%,transparent 55%, transparent 100%),
+          radial-gradient(circle at 65% 91%, hsla(329,0%,99%,0.05) 0%, hsla(329,0%,99%,0.05) 46%,transparent 46%, transparent 76%,transparent 76%, transparent 100%),
+          linear-gradient(45deg, rgb(101,114,75),rgb(89,125,32))`,
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
